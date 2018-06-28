4b594fa8cacee4421179366eb7205b51b9682b2b
Chiti Simbotwe <chitis@fb.com>
2018-06-28 16:28:34 -0700

    Initial commit

---
+++ app/src/main/java/com/example/chitis/flixster/models/JSON0bject.java
@@ -0,0 +1,4 @@
+package com.example.chitis.flixster.models;
+
+class JSON0bject {
+}
---
+++ app/src/main/java/com/example/chitis/flixster/models/Movie.java
@@ -0,0 +1,32 @@
+package com.example.chitis.flixster.models;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+
+public class Movie {
+
+    // values from api
+    private String title;
+    private String overview;
+    private String posterPath; // only the path
+
+    // initialize from the JSON data
+    public Movie(JSONObject object) throws JSONException {
+        title = object.getString("title");
+        overview = object.getString("overview");
+        posterPath = object.getString("poster_path");
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getOverview() {
+        return overview;
+    }
+
+    public String getPosterPath() {
+        return posterPath;
+    }
+}
